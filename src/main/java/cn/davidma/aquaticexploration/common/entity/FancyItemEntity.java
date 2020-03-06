95cbbfaabf6f0a902876f4e7bd2dcb1bedf05007
BuildTools <davidmaamoaix@gmail.com>
2020-03-06 22:15:04 +0800

    Fixes

--- src/main/java/cn/davidma/aquaticexploration/common/entity/FancyItemEntity.java
+++ src/main/java/cn/davidma/aquaticexploration/common/entity/FancyItemEntity.java
@@ -22,11 +22,15 @@ public class FancyItemEntity extends ItemEntity {
 	}
 	
 	public FancyItemEntity(FMLPlayMessages.SpawnEntity packet, World world) {
-		super(AquaticEntities.FANCY_ITEM.get(), world);
+		this(AquaticEntities.FANCY_ITEM.get(), world);
 	}
 	
 	public FancyItemEntity(World world, double x, double y, double z, ItemStack stack) {
-		super(world,  x, y, z, stack);
+		this(AquaticEntities.FANCY_ITEM.get(), world);
+		this.setItem(stack);
+		this.rotationYaw = this.rand.nextFloat() * 360.0F;
+		this.setMotion(this.rand.nextDouble() * 0.2 - 0.1, 0.2, this.rand.nextDouble() * 0.2 - 0.1);
+		this.lifespan = (stack.getItem() == null ? 6000 : stack.getEntityLifespan(world));
 	}
 	
 	public FancyItemEntity setInvulnerable(DamageSource... sources) {
